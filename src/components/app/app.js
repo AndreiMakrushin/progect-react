e32fd0683f35023f6ec3861ed4544ea3e0d56e74
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-10-06 03:44:05 +0500

    законченый проект

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,7 +14,9 @@ class App extends Component{
                 {name: 'Andrey', salary: '6000', increase: false, like:false, id: 1} ,
                 {name: 'Alena', salary: '8000', increase: false, like:false, id: 2},
                 {name: 'Di', salary: '5000', increase: false, like:false, id: 3}
-            ]
+            ],
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     };
@@ -61,15 +63,43 @@ class App extends Component{
             })
         }))
     }
+
+    searchEmp = (items, term) =>{
+        if (term.length === 0){
+            return items;
+        }
+        return items.filter(i =>{
+            return i.name.indexOf(term) > - 1
+        })
+    }
+
+    onUpdateSearch = (term)=>{
+        this.setState({term})
+    }
+    filterPost = (items, filter) =>{
+        switch (filter) {
+            case 'like':
+                return items.filter(i => i.like);
+            case 'moreThen1000':
+                return items.filter(i => i.salary > 1000);
+            default:
+                return items
+        }
+    }
+    onFilterSelect = (filter) =>{
+        this.setState({filter})
+    }
     render(){
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(i=>i.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
         return (
             <div className="app">
                 <AppInfo employees={employees} increased={increased}/>
-                <div className='search-panel'><SearchPanel /><AppFilter /></div>
+                <div className='search-panel'><SearchPanel onUpdateSearch={this.onUpdateSearch}/><AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/></div>
                 <EmployersAddForm onAdd={this.addItem}/>
-                <EmployersList data={this.state.data} onDelete={this.deleteItem} onToggleIncrease={this.onToggleIncrease} onToggleLike={this.onToggleLike}/>
+                <EmployersList data={visibleData} onDelete={this.deleteItem} onToggleIncrease={this.onToggleIncrease} onToggleLike={this.onToggleLike}/>
                 
             </div>
         )
